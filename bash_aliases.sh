2e1fa5811a29bbed81ed63e28a1705a0e396246c
Charlietje <verstraete.carl@gmail.com>
2017-11-15 09:58:08 +0100

    added transfer file sharing

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -85,3 +85,20 @@ function cheat() {
 function qr() {
     curl http://qrenco.de/"$1"
 }
+
+function transfer() {
+    if [ $# -eq 0  ]; then
+        echo -e "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"
+        return 1
+    fi
+    tmpfile=$( mktemp -t transferXXX  )
+    if tty -s; then
+        basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g')
+        curl --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile
+    else
+        curl --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile
+    fi
+    cat $tmpfile
+    echo
+    rm -f $tmpfile
+}
