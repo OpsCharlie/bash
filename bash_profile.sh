27cc86049fdd57d2e087cddc4ebf1ac11680579f
Charlietje <verstraete.carl@gmail.com>
2018-01-31 15:15:29 +0100

    Removed inxi

--- bash_profile.sh
+++ bash_profile.sh
@@ -8,10 +8,10 @@ mesg n
 
 # run inxi information tool
 if [ -x "`which inxi 2>&1`" ]; then
-    inxi -ISi -v0 -c5
+    # inxi -ISi -v0 -c5
     #inxi -IpRS -v0 -c5
 # else
-    # [[ -f /var/run/motd.dynamic ]] && cat /var/run/motd.dynamic
+    [[ -f /var/run/motd.dynamic ]] && cat /var/run/motd.dynamic
 fi
 
 echo
